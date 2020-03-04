7365bd905756b8135a9d15b92afd987e806dd2b7
Justin Hendrickson <justintunev7@gmail.com>
2020-03-04 14:40:08 -0700

    decision tree mostly working (part 1)

--- desiciontree/__init__.py
+++ desiciontree/__init__.py
@@ -1,1 +1,49 @@
+from arff import Arff
+import matplotlib.pyplot as plt
+import numpy as np
+from desiciontree import DTClassifier
+from sklearn.model_selection import train_test_split
+import sys
+import math
 
+def split_data(X, y, test_split=.25):
+    # create test split
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_split)
+    return X_train, X_test, y_train, y_test
+
+def debug():
+    mat = Arff("./lenses.arff",label_count=1)
+    counts = [] ## this is so you know how many types for each column
+
+    for i in range(mat.data.shape[1]):
+        counts += [mat.unique_value_count(i)]
+    data = mat.data[:,0:-1]
+    labels = mat.data[:,-1]
+    DTClass = DTClassifier(counts)
+    DTClass.fit(data,labels).get_root().print_tree()
+    mat2 = Arff("./lenses.arff", label_count=1)
+    data2 = mat2.data[:,0:-1]
+    labels2 = mat2.data[:,-1]
+    pred = DTClass.predict(data2)
+    print("FINISHED!")
+    Acc = DTClass.score(data2,labels2)
+
+    # write to CSV file
+    np.savetxt("pred_lenses.csv",pred,delimiter=",")
+    print("Accuracy = [{:.2f}]".format(Acc))
+
+def main():
+    print("Starting tests")
+    debug()
+    # part_one()
+    # part_two()
+    # part_three()
+    # part_four()
+    # part_five()
+    # part_six()
+    return
+
+
+
+if __name__ == '__main__':
+    main()
--- desiciontree/desiciontree.py
+++ desiciontree/desiciontree.py
@@ -1,14 +1,62 @@
 import numpy as np
+import math
 from sklearn.base import BaseEstimator, ClassifierMixin
 
-### NOTE: The only methods you are required to have are:
+
+class Node():
+    def __init__(self, feature=None, value=None, data=[]):
+        self.feature = feature
+        self.value = value
+        self.children = []
+        print(data)
+        self.data = data
+
+    # feature is index of feature, classes is list of class indices for feature
+    def split(self, feature, classes, data):
+        for i in range(classes):
+            self.children.append(Node(int(feature), int(i), data[i]))
+        
+    def get_feature_value(self):
+        return self.feature, self.value
+    
+    def get_children(self):
+        return self.children
+    
+    def get_data(self):
+        return self.data
+    
+    def output(self):
+        a = list(self.data[:,-1])
+        print(a)
+        return max(map(lambda val: (a.count(val), val), set(a)))[-1]
+    
+    def predict(self, instance):
+        # print(len(self.children), len(self.data))
+        if len(self.children) == 0:
+            output = self.output()
+            print(output)
+            return output
+        child_feature, child_val = self.children[0].get_feature_value()
+        return self.children[int(instance[child_feature])].predict(instance)
+    
+    def print_tree(self, pre=""):
+        # print(pre, self.feature_class)
+        for child in self.children:
+            print(pre, child.get_feature_value(), "\n", pre, child.get_data(), "\n\n")
+        for child in self.children:
+            child.print_tree(pre + "\t")
+
+# NOTE: The only methods you are required to have are:
 #   * predict
 #   * fit
 #   * score
-
-class DTClassifier(BaseEstimator,ClassifierMixin)::
-
-    def __init__(self,counts=None):
+class DTClassifier(BaseEstimator, ClassifierMixin):
+    def __init__(self, counts=None):
+        self.counts = counts
+        print("COUNTS: ", counts)
+        self.weights = None
+        self.root = Node()
+        self.visited = []
         """ Initialize class with chosen hyperparameters.
 
         Args:
@@ -16,8 +64,92 @@ class DTClassifier(BaseEstimator,ClassifierMixin)::
         Example:
             DT  = DTClassifier()
         """
+    
+    def init_weights(self):
+        if self.weights: return [w * 0 for w in self.weights] # map(lambda w: w * 0, self.weights)
+        self.weights = []
+        for i in range(len(self.counts) - 1):
+            self.weights.append(np.zeros((self.counts[i], self.counts[-1])))
+        return self.weights
+    
+    def feature_entropy(self, data, total):
+        entropy = 0
+        for i in range(len(data)):
+            feature_sum = data[i].sum()
+            if feature_sum == 0: continue
+            feature_prob = (feature_sum / total)
+            feature_entropy = 0
+            for j in range(len(data[i])):
+                prob = (data[i][j]) / feature_sum
+                if prob > 0: feature_entropy += prob * math.log2(prob)
+            entropy += feature_entropy * (-1) * (feature_prob)
+        return entropy
 
-    def fit(self, X, y):
+    # Get the entropy of a given attribute split
+    # data is a 2D matrix that represents the count of each
+    # feature class to target class. Targets is an array of counts of target classes
+    def entropy(self, targets, data=None):
+        total = targets.sum()
+        if data is None:
+            entropy = 0
+            for i in range(len(targets)):
+                prob = (targets[i] / total)
+                entropy += prob * math.log2(prob)
+            return entropy * -1
+        else:
+            return self.feature_entropy(data, total)
+    
+    def split_data(self, X, y, feature):
+        data = []
+        combined = np.column_stack((X, y))
+        for i in range(self.counts[feature]):
+            print("SPLITTING:",i)
+            data.append(combined[combined[:,feature] == i])
+        return data
+
+    def get_min_entropy(self, targets, visited=[]):
+        min_entropy = math.inf
+        min_entropy_index = -1
+        for i, matrix in enumerate(self.weights):
+            if i in visited: continue
+            temp_entropy = self.entropy(targets, matrix)
+            if temp_entropy < min_entropy: 
+                min_entropy = temp_entropy
+                min_entropy_index = i
+        return min_entropy_index
+
+    def fit(self, X, y, node=None, visited=[]):
+        print("IN FIT", visited)
+        if node is None: node = self.root
+        self.weights = self.init_weights()
+        targets = np.zeros((self.counts[-1]))
+        # get feature-output counts
+        for i, instance in enumerate(X):
+            for j, feature in enumerate(instance):
+                self.weights[j][int(feature), int(y[i])] += 1
+            targets[int(y[i])] += 1
+        
+        min_entropy_index = self.get_min_entropy(targets, targets)
+
+        
+        data = self.split_data(X, y, min_entropy_index)
+        node.split(min_entropy_index, self.counts[min_entropy_index], data)
+
+        # print("\n\nTREE")
+        # self.root.print_tree()
+
+        for i, matrix in enumerate(data):
+            # self.fit(matrix[:,:-1], matrix[:,-1], node.get_children()[i], visited + [i])
+            if i in visited or len(matrix) == 0: continue
+            remaining = list(range(len(matrix[0][:-1])))
+            # print("REMAINING FEATURES: ", remaining, i)
+            # del remaining[i]
+            self.fit(matrix[:,remaining], matrix[:,-1], node.get_children()[i], visited + [i])
+        
+            # feature_entropy[i] = self.entropy(targets, matrix)
+        
+
+
         """ Fit the data; Make the Desicion tree
 
         Args:
@@ -30,8 +162,17 @@ class DTClassifier(BaseEstimator,ClassifierMixin)::
         """
 
         return self
+    
+    def predict_with_tree(self, instance):
+        self.root.predict(instance)
 
     def predict(self, X):
+        y_hat = []
+        for i, instance in enumerate(X):
+            # print("\n\n", i)
+            y_hat.append(self.root.predict(instance))
+        print(y_hat)
+        return y_hat
         """ Predict all classes for a dataset X
 
         Args:
@@ -43,13 +184,18 @@ class DTClassifier(BaseEstimator,ClassifierMixin)::
         """
         pass
 
-
     def score(self, X, y):
+        y_hat = self.predict(X)
+        sum = 0
+        for i, result in enumerate(y):
+            sum += 0 if result == y_hat[i] else 1
+        return 1 - sum / len(y_hat)
         """ Return accuracy of model on a given dataset. Must implement own score function.
 
         Args:
             X (array-like): A 2D numpy array with data, excluding targets
             y (array-li    def _shuffle_data(self, X, y):
         """
-        return 0
 
+    def get_root(self):
+        return self.root
