b9b7faca0e5230d227729f141ecedcd914f6c2db
Justin Hendrickson <justintunev7@gmail.com>
2020-03-18 17:38:05 -0600

    KNN: part 4 done

--- KNN/KNN.py
+++ KNN/KNN.py
@@ -5,7 +5,7 @@ from sklearn.base import BaseEstimator, ClassifierMixin
 class KNNClassifier(BaseEstimator,ClassifierMixin):
 
 
-    def __init__(self,labeltype=[],weight_type='inverse_distance',columntype="nominal",k=3, use_distance_weighting=False): ## add parameters here
+    def __init__(self,labeltype=[],weight_type='inverse_distance',columntype="nominal",k=3, use_distance_weighting=False, regression=False): ## add parameters here
         """
         Args:
             columntype for each column tells you if continues[real] or if nominal.
@@ -15,6 +15,7 @@ class KNNClassifier(BaseEstimator,ClassifierMixin):
         self.use_distance_weighting = use_distance_weighting
         self.weight_type = weight_type
         self.k = k
+        self.regression = regression
 
 
 
@@ -27,27 +28,52 @@ class KNNClassifier(BaseEstimator,ClassifierMixin):
             self: this allows this to be chained, e.g. model.fit(X,y).predict(X_test)
         """
         self.data = np.array(data)
-        self.labels = labels.astype(int)
-        self.classes = max(self.labels) + 1
+        self.labels = labels
         return self
     
     # Gets the sorted distances between the row and all other datapoints in self.data
     # returns: sorted array of tuples of (distance, index, label)
     def getDistances(self, row):
         distances = np.column_stack((np.linalg.norm(self.data-row, axis=-1), self.labels))
+        # distances = np.column_stack(((np.sqrt(np.sum(np.power(self.data-row, 2), axis=-1))), self.labels))
         return distances[distances[:,0].argpartition(kth=self.k)][0:self.k]
 
-    def predict_label(self, neighbors):
+    # Computes regression labels
+    def predict_regression(self, neighbors):
         if self.use_distance_weighting:
-            labels = {}
-            for i in range(self.classes):
-                print(i)
-                labels[i] = 0
-            for i, neighbor in enumerate(neighbors):
-                if neighbor[0] == 0.0: return int(neighbor[-1])
-                if neighbor[-1] not in labels: print(labels)
-                labels[int(neighbor[-1])] += (1 / (neighbor[0]))         
-            return max(labels,key=labels.get)
+            # If distance is zero, return that label
+            if 0.0 in neighbors[:,0]:
+                return neighbors[np.where(neighbors == 0.0)[0][0], -1]
+
+            # Regression formula
+            return np.sum((neighbors[:,-1] / (neighbors[:,0] ** 2))) / np.sum((1 / (neighbors[:,0] ** 2)))
+        elif self.regression:
+            # if we don't care about the distance weighting, then just return the average label
+            return np.mean(neighbors[:,-1])
+
+    # Predicts using the distances (1/d^2) for each neighbor
+    def predict_distance_weighted(self, neighbors):
+        self.classes = int(max(self.labels) + 1)
+        # Initialize labels (this helps with edge cases when two labels have equal distances)
+        labels = {}
+        for i in range(self.classes):
+            labels[i] = 0
+        for i, neighbor in enumerate(neighbors):
+            # If distance is zero, just return the label
+            if neighbor[0] == 0.0:
+                return int(neighbor[-1])
+            # Error check
+            if neighbor[-1] not in labels: print(labels)
+            # inverse distance = 1/d^2
+            labels[neighbor[-1]] += (1 / (neighbor[0] ** 2))
+        return max(labels,key=labels.get)
+
+    # Generic predict method
+    def predict_label(self, neighbors):
+        if self.regression:
+            return self.predict_regression(neighbors)
+        elif self.use_distance_weighting:
+            return self.predict_distance_weighted(neighbors)
         else:
             # get max counted class
             counts = np.bincount(neighbors.astype(int)[:,-1])
@@ -59,7 +85,9 @@ class KNNClassifier(BaseEstimator,ClassifierMixin):
             # if i % 50 == 0:
             #     print(i)
             neighbors = self.getDistances(row)
+            # print(neighbors)
             y_hat.append(self.predict_label(neighbors))
+        # print(y_hat)
         return y_hat
 
 
@@ -70,14 +98,17 @@ class KNNClassifier(BaseEstimator,ClassifierMixin):
             array, shape (n_samples,)
                 Predicted target values per element in X.
         """
-        pass
     
     # returns the mean squared error on the data set
     def mse(self, y_hat, y_truth):
+        # print(y_hat)
+        # print(y_truth)
+        # print(y_hat - y_truth)
         return np.square(y_hat - y_truth).mean(axis=None)
 
     # Accuracy based on percent correct
     def accuracy(self, y_hat, y_truth):
+        # print(y_hat, y_truth)
         sum = 0
         for i, result in enumerate(y_truth):
             sum += 0 if result == y_hat[i] else 1
@@ -86,8 +117,8 @@ class KNNClassifier(BaseEstimator,ClassifierMixin):
     #Returns the Mean score given input data and labels
     def score(self, X, y):
         y_hat = self.predict(X)
-        error = self.mse(y_hat, y.astype(int))
-        accuracy = self.accuracy(y_hat, y.astype(int))
+        error = self.mse(y_hat, y)
+        accuracy = self.accuracy(y_hat, y)
         return error, accuracy 
         
         """ Return accuracy of model on a given dataset. Must implement own score function.
--- KNN/__init__.py
+++ KNN/__init__.py
@@ -7,6 +7,22 @@ import sys
 import copy
 import math
 
+def plot_bar(data, labels=None, xlabel="", ylabel="", title=""):
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    plt.title(title)
+    if labels != None: plt.xticks(range(len(labels)), labels)
+    if type(data) == dict:
+        width = 1 / (len(data) + 1)
+        for i, key in enumerate(data):
+            value = data[key]
+            offset = width * (i - len(data) // 2)
+            plt.bar(np.arange(len(value)) + offset, value, width=width, label=key) 
+        plt.legend()
+    else:
+        plt.bar(range(len(data)), data) 
+    plt.show()
+
 def setup_data(file, label_count=1):
     print("\nFile =",file)
     mat = Arff(file,label_count=label_count)
@@ -15,6 +31,12 @@ def setup_data(file, label_count=1):
     data = raw_data[:,:-1]
     labels = raw_data[:,-1]
     return data, labels
+
+def split_data(X, y, test_split=.25):
+    # create test split
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_split)
+    return X_train, X_test, y_train, y_test
+
 
 def normalize(data):
     maxes = data.max(axis=1)
@@ -27,51 +49,119 @@ def split_data(X, y, test_split=.25):
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_split)
     return X_train, X_test, y_train, y_test
 
-def two():
+def five():
+    data, labels = setup_data("credit_approval.arff")
+    X_train, X_test, y_train, y_test = split_data(data, labels, test_split=.20)
+    norm_train_data = normalize(X_train)
+    norm_test_data = normalize(X_test)
+    KNN = KNNClassifier(labeltype ='classification',weight_type='inverse_distance', k=7, use_distance_weighting=False, regression=True)
+    KNN.fit(norm_train_data,y_train)
+    pred = KNN.predict(norm_test_data)
+    error, accuracy = KNN.score(norm_test_data,y_test)
+    print("ERROR:", error)
+    print("ACCURACY:", accuracy)
+
+def four():
+    # house_price
+    train_data, train_labels = setup_data("house_price_train.arff")
+    test_data, test_labels = setup_data("house_price_test.arff")
+    norm_train_data = normalize(train_data)
+    norm_test_data = normalize(test_data)
+
+    _, mse, labels = try_k(norm_train_data, norm_test_data, train_labels, test_labels, reg=True, use_distance_weighting=True)    
+    plot_bar(mse, labels, xlabel="k", ylabel="MSE", title="House Price MSE by K value")
+
+    # magic_telescope
     train_data, train_labels = setup_data("magic_telescope_train.arff")
     test_data, test_labels = setup_data("magic_telescope_test.arff")
-    # without normalizing
-    # KNN = KNNClassifier(labeltype ='classification',weight_type='inverse_distance', k=3)
-    # KNN.fit(train_data,train_labels)
-    # pred = KNN.predict(test_data)
-    # error, accuracy = KNN.score(test_data,test_labels)
-    # print("ERROR:", error)
-    # print("ACCURACY:", accuracy)
+    norm_train_data = normalize(train_data)
+    norm_test_data = normalize(test_data)
 
+    _, mse, labels = try_k(norm_train_data, norm_test_data, train_labels, test_labels, reg=True, use_distance_weighting=True)    
+    plot_bar(mse, labels, xlabel="k", ylabel="MSE", title="Magic Telescope MSE by K value")
+
+
+def three():
+    train_data, train_labels = setup_data("house_price_train.arff")
+    test_data, test_labels = setup_data("house_price_test.arff")
     # normalize
     norm_train_data = normalize(train_data)
     norm_test_data = normalize(test_data)
-    KNN = KNNClassifier(labeltype ='classification',weight_type='inverse_distance', k=3)
+
+    KNN = KNNClassifier(labeltype ='classification',k=3, use_distance_weighting=False, regression=True)
     KNN.fit(norm_train_data,train_labels)
     pred = KNN.predict(norm_test_data)
     error, accuracy = KNN.score(norm_test_data,test_labels)
     print("NORMALIZED ERROR:", error)
     print("NORMALIZED ACCURACY:", accuracy)
 
+    _, mse, labels = try_k(norm_train_data, norm_test_data, train_labels, test_labels, reg=True)  
+    plot_bar(mse, labels, xlabel="k", ylabel="MSE", title="Housing Price KNN MSE by K value")
 
-    k = 3
-    while k <= 15:
-        KNN = KNNClassifier(labeltype ='classification',weight_type='inverse_distance', k=k)
-        KNN.fit(norm_train_data,train_labels)
-        pred = KNN.predict(norm_test_data)
-        error, accuracy = KNN.score(norm_test_data,test_labels)
-        print("NORMALIZED ERROR for k =", k, ":", error)
-        print("NORMALIZED ACCURACY for k =", k, ":", accuracy)
-        k += 2
 
-def one():
-    train_data, train_labels = setup_data("diabetes.arff")
-    test_data, test_labels = setup_data("diabetes_test.arff")
-    KNN = KNNClassifier(labeltype ='classification',weight_type='inverse_distance', k=3)
+def two():
+    train_data, train_labels = setup_data("magic_telescope_train.arff")
+    test_data, test_labels = setup_data("magic_telescope_test.arff")
+    # # without normalizing
+    KNN = KNNClassifier(labeltype ='classification',weight_type='inverse_distance', k=3, use_distance_weighting=False)
     KNN.fit(train_data,train_labels)
     pred = KNN.predict(test_data)
     error, accuracy = KNN.score(test_data,test_labels)
     print("ERROR:", error)
     print("ACCURACY:", accuracy)
-    # print("PREDICTION",pred)
-    np.savetxt("seismic-bump-prediction.csv",pred,delimiter=',',fmt="%i")
+
+    # normalize
+    norm_train_data = normalize(train_data)
+    norm_test_data = normalize(test_data)
+    KNN = KNNClassifier(labeltype ='classification',weight_type='inverse_distance', k=3, use_distance_weighting=False)
+    KNN.fit(norm_train_data,train_labels)
+    pred = KNN.predict(norm_test_data)
+    error, accuracy = KNN.score(norm_test_data,test_labels)
+    print("\nNORMALIZED ERROR:", error)
+    print("NORMALIZED ACCURACY:", accuracy)
+
+    accuracies, _, labels = try_k(norm_train_data, norm_test_data, train_labels, test_labels)
+    plot_bar(accuracies, labels, xlabel="k", ylabel="Accuracy", title="Magic Telescope KNN Accuracy by K value")
+
+def try_k(norm_train_data, norm_test_data, train_labels, test_labels, reg=False, use_distance_weighting=False):
+    k = 1
+    accuracies = []
+    mse = []
+    labels = []
+    while k <= 15:
+        KNN = KNNClassifier(labeltype ='classification',weight_type='inverse_distance', k=k, use_distance_weighting=use_distance_weighting, regression=reg)
+        KNN.fit(norm_train_data,train_labels)
+        pred = KNN.predict(norm_test_data)
+        error, accuracy = KNN.score(norm_test_data,test_labels)
+        print("\nNORMALIZED ERROR for k =", k, ":", error)
+        print("NORMALIZED ACCURACY for k =", k, ":", accuracy)
+        accuracies.append(accuracy)
+        mse.append(error)
+        labels.append(k)
+        k += 2
+    return accuracies, mse, labels
+
+def one():
+    train_data, train_labels = setup_data("diabetes.arff")
+    test_data, test_labels = setup_data("diabetes_test.arff")
+    KNN = KNNClassifier(labeltype ='classification',weight_type='inverse_distance', k=3, use_distance_weighting=True)
+    KNN.fit(train_data,train_labels)
+    pred = KNN.predict(test_data)
+    error, accuracy = KNN.score(test_data,test_labels)
+    print("ERROR:", error)
+    print("ACCURACY:", accuracy)
+    np.savetxt("diabetes-prediction.csv",pred,delimiter=',',fmt="%i")
 
 def debug():
+    train_data, train_labels = setup_data("seismic-bumps_train.arff")
+    test_data, test_labels = setup_data("seismic-bumps_test.arff")
+    KNN = KNNClassifier(labeltype ='classification',weight_type='inverse_distance', k=3, use_distance_weighting=False)
+    KNN.fit(train_data,train_labels)
+    pred = KNN.predict(test_data)
+    error, accuracy = KNN.score(test_data,test_labels)
+    print("(without weighting) ERROR:", error)
+    print("(without weighting) ACCURACY:", accuracy)
+
     train_data, train_labels = setup_data("seismic-bumps_train.arff")
     test_data, test_labels = setup_data("seismic-bumps_test.arff")
     KNN = KNNClassifier(labeltype ='classification',weight_type='inverse_distance', k=3, use_distance_weighting=True)
@@ -86,12 +176,12 @@ def debug():
 
 def main():
     print("Starting tests")
-    debug()
+    # debug()
+    # one()
     # two()
-    # part_two()
-    # part_three()
-    # part_five()
-    # part_six()
+    # three()
+    # four()
+    five()
     return
 
 
