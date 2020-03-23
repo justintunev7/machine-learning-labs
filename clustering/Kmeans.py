b8f0fb261d3eaefeb7e114344d4a1f078b07e45a
Justin Hendrickson <justintunev7@gmail.com>
2020-03-23 16:45:49 -0600

    kmeans clustering done (debug validated)

--- clustering/Kmeans.py
+++ clustering/Kmeans.py
@@ -1,5 +1,7 @@
 import numpy as np
 from sklearn.base import BaseEstimator, ClusterMixin
+from random import randint
+from copy import copy
 
 class KMEANSClustering(BaseEstimator,ClusterMixin):
 
@@ -11,8 +13,54 @@ class KMEANSClustering(BaseEstimator,ClusterMixin):
         """
         self.k = k
         self.debug = debug
+    
+    def init_centroids(self, X):
+        centroids = []
+        if self.debug:
+            centroids = X[:self.k]
+        else:
+            selected = set(())
+            for i in range(self.k):
+                rand_index = randint(0, len(X))
+                while rand_index in selected: rand_index = randint(0, len(X))
+                selected.add(rand_index)
+                centroids.append(X[rand_index])
+        return centroids
+    
+    def getDistanceFromCentroid(self, centroid):
+        return np.linalg.norm(self.data-centroid, axis=-1)
+
+    def getDistances(self, centroids):
+        distances = np.empty((0,len(self.data)))
+        for centroid in centroids:
+            distances = np.vstack((distances, self.getDistanceFromCentroid(centroid)))
+        return distances
+
+    def getClusters(self, distances):
+        clusters = [[] for i in range(self.k)]
+        clusters_indices = np.argmin(distances, axis=0)
+        for i, cluster in enumerate(clusters_indices):
+            clusters[cluster].append(self.data[i])
+        return np.array(clusters)
+    
+    def getNewCentroids(self, distances):
+        self.clusters = self.getClusters(distances)
+        centroids = []
+        for cluster_val in self.clusters:
+            centroids.append(np.mean(cluster_val, axis=0))
+        return np.array(centroids)
+
 
     def fit(self,X,y=None):
+        self.data = np.array(X)
+        self.centroids = self.init_centroids(X)
+        prev_centroids = []
+
+        while not np.array_equal(prev_centroids, self.centroids):
+            prev_centroids = copy(self.centroids)
+            distances = self.getDistances(self.centroids)
+            self.centroids = self.getNewCentroids(distances)
+        
         """ Fit the data; In this lab this will make the K clusters :D
         Args:
             X (array-like): A 2D numpy array with the training data
@@ -21,16 +69,25 @@ class KMEANSClustering(BaseEstimator,ClusterMixin):
             self: this allows this to be chained, e.g. model.fit(X,y).predict(X_test)
         """
         return self
+    
+    # get SSE of clusters
+    def sse(self):
+        self.sse = []
+        for i, centroid in enumerate(self.centroids):
+            self.sse.append(self.clusterSSE(centroid, self.clusters[i]))
+        return np.array(self.sse)
+    
+    def clusterSSE(self, centroid, cluster):
+        return np.square(cluster - centroid).sum(axis=None)
+
     def save_clusters(self,filename):
-        """
-            f = open(filename,"w+") 
-            Used for grading.
-            write("{:d}\n".format(k))
-            write("{:.4f}\n\n".format(total SSE))
-            for each cluster and centroid:
-                write(np.array2string(centroid,precision=4,separator=","))
-                write("\n")
-                write("{:d}\n".format(size of cluster))
-                write("{:.4f}\n\n".format(SSE of cluster))
-            f.close()
-        """
+        sse = self.sse()
+        f = open(filename,"w+")
+        f.write("{:d}\n".format(self.k))
+        f.write("{:.4f}\n\n".format(sse.sum()))
+        for i, centroid in enumerate(self.centroids):
+            f.write(np.array2string(centroid,precision=4,separator=","))
+            f.write("\n")
+            f.write("{:d}\n".format(len(self.clusters[i])))
+            f.write("{:.4f}\n\n".format(sse[i]))
+        f.close()
---
+++ clustering/__init__.py
@@ -0,0 +1,97 @@
+from arff import Arff
+import matplotlib.pyplot as plt
+import numpy as np
+from HAC import HACClustering
+from Kmeans import KMEANSClustering
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.model_selection import train_test_split
+from sklearn.decomposition import PCA
+import sys
+import copy
+import math
+
+
+def plot_bar(data, labels=None, xlabel="", ylabel="", title=""):
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    plt.title(title)
+    if labels != None:
+        plt.xticks(range(len(labels)), labels)
+    if type(data) == dict:
+        width = 1 / (len(data) + 1)
+        for i, key in enumerate(data):
+            value = data[key]
+            offset = width * (i - len(data) // 2)
+            plt.bar(np.arange(len(value)) + offset,
+                    value, width=width, label=key)
+        plt.legend()
+    else:
+        plt.bar(range(len(data)), data)
+    plt.show()
+
+
+ ## label_count = 0 because clustering is unsupervised.
+def setup_data(file, label_count=0):
+    print("\nFile =", file)
+    mat = Arff(file, label_count=label_count)
+    raw_data = mat.data
+    data = raw_data
+    scaler = MinMaxScaler()
+    scaler.fit(data)
+    norm_data = scaler.transform(data)
+    return norm_data
+
+
+def split_data(X, y, test_split=.25):
+    # create test split
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=test_split)
+    return X_train, X_test, y_train, y_test
+
+
+def normalize(data):
+    maxes = data.max(axis=1)
+    mins = data.min(axis=1)
+    normal = (data - mins[:, None]) / (maxes - mins)[:, None]
+    return normal
+
+
+def split_data(X, y, test_split=.25):
+    # create test split
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=test_split)
+    return X_train, X_test, y_train, y_test
+
+def kmeans(norm_data, k=5, debug=False, output_file="debug_kmeans.txt"):
+    KMEANS = KMEANSClustering(k=k,debug=debug)
+    KMEANS.fit(norm_data)
+    KMEANS.save_clusters(output_file)
+
+### HAC SINGLE LINK ###
+# link_type = 'single' or 'complete'
+# output_file = debug_hac_complete.txt or debug_hac_single.txt or whatever
+def hac_single(norm_data, link_type='single', output_file="debug_hac_single.txt"):
+    HAC_single = HACClustering(k=5,link_type=link_type)
+    HAC_single.fit(norm_data)
+    HAC_single.save_clusters(output_file)
+
+def debug():
+    data = setup_data('abalone.arff')
+    kmeans(data, debug=True)
+
+
+def main():
+    print("Starting tests")
+    debug()
+    # one()
+    # two()
+    # three()
+    # four()
+    # five()
+    # six()
+    # seven()
+    return
+
+
+if __name__ == '__main__':
+    main()
